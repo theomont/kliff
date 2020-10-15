9d9dfba6f0836e7f5d16ebd82c969a3e01ce0e97
Theo <theomon@gmail.com>
2020-10-15 10:58:38 -0300

    Put SECRET_KEY explicitly on file

--- kliff_online/kliff_online/settings.py
+++ kliff_online/kliff_online/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import keys
+#import keys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = keys.PK
+#SECRET_KEY = keys.PK
+SECRET_KEY = '=#yz=u^q#eps^i2h61pf01@v37q-^rd5z6zr4b0tys8pa1if_u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
